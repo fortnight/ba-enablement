2ccb75c21e5f1172168da6357e41b4215c2b703c
Fortnight <nellis@redhat.com>
2015-08-25 10:01:18 -0400

    Did a maven archetype generate to create a basic hello-world

---
+++ simple/src/main/java/org/fortnight/bpms/App.java
@@ -0,0 +1,13 @@
+package org.fortnight.bpms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
